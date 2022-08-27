1c70265852e1a81d10030d815caf5e08e086defa
Neru9898 <neonerushan@gmail.com>
2022-08-27 15:00:25 -0400

    create get route

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+let list = [];
+
+app.get("/", (req, res) => {
+  res.status(201).send({ success: true, person: list });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
