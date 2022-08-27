16a87870083938a2ae69dddfc64b2e5b245643b4
Neru9898 <neonerushan@gmail.com>
2022-08-27 15:16:31 -0400

    added post request

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 let list = [];
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.status(201).send({ success: true, person: list });
+  res.status(200).send({ success: true, person: list });
+});
+
+app.post("/", (req, res) => {
+  const data = req.body;
+  list = list.concat(data);
+  res.status(200).send({ success: true, person: list });
 });
 
 app.listen(PORT, () => {
